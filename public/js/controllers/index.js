782f9a4018618fc0615b7857848a0f7d918a77bf
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-03 12:42:14 -0700

    Split up question/answer cards to be handled by different functions

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,4 +3,34 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
     socket.on('test', function() {
       console.log('test!');
     });
+}]).controller('CardController', ['$scope', 'ApiService', function($scope, ApiService) {
+  ApiService.getCards()
+    .then(function(data) {
+       angular.forEach(data, function(data, key){
+        if (data.numAnswers) {
+          answers(data);
+        } else if (!data.numAnswers) {
+          questions(data);
+        }
+      });
+    });
+
+    var answers = function(answerCards) {
+      
+    };
+
+    var questions = function(questionCards) {
+
+    };
 }]);
+
+/**
+data.id -> card number
+data.text -> card text
+data.expansion -> if it's from the base card set or an expansion pack
+--> types available -
+---- Base, CAHe1, CAHe2, CAHgrognards, CAHweeaboo, CAHxmas, NEIndy,
+---- NSFH, CAHe3, Image1, GOT, PAXP13
+data.numAnswers --> identifies if it's a question card and how many
+answers it accepts
+**/
