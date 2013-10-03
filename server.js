855a5edd74d07fc09dbe8abe83a3be2571466aaf
William Ngo <will@mrngoitall.net>
2013-10-02 19:20:32 -0700

    Moved DB host connection string to environment variable to enable authentication.

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development',
     mongoose = require('mongoose');
 
 //Bootstrap db connection
-var db = mongoose.connect(config.db);
+var db_host = process.env.C4H_DB_HOST || "mongodb://localhost/mean-dev";
+var db = mongoose.connect(db_host);
 
 //Bootstrap models
 var models_path = __dirname + '/app/models';
