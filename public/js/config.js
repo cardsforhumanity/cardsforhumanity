c4498ed5448247317cd6a52ba81285f00e269ce8
Hack Reactor Students <github@hackreactor.com>
2013-10-02 20:29:32 -0700

    Adding Socket.io infrastructure to project

--- public/js/config.js
+++ public/js/config.js
@@ -17,3 +17,17 @@ window.app.config(['$locationProvider',
         $locationProvider.hashPrefix("!");
     }
 ]);
+
+//Setting up SafeApply on rootScope
+window.app.run(['$rootScope', function($rootScope) {
+  $rootScope.safeApply = function(fn) {
+    var phase = this.$root.$$phase;
+    if(phase == '$apply' || phase == '$digest') {
+        if(fn && (typeof(fn) === 'function')) {
+            fn();
+        }
+    } else {
+        this.$apply(fn);
+      }
+  };
+}]);
