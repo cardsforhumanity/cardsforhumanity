6e9caf544f0f5809289a4d1f8fe18afb6a9c6011
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-03 11:04:31 -0700

    Added factory service for http get requests

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,5 @@
-angular.module('mean.system').factory("Global", [function() {
+angular.module('mean.system')
+  .factory('Global', [function() {
     var _this = this;
     _this._data = {
         user: window.user,
@@ -6,4 +7,20 @@ angular.module('mean.system').factory("Global", [function() {
     };
 
     return _this._data;
+}]).factory('ApiService', ['$http', '$q', function($http, $q) {
+  return {
+    getCards: function() {
+      return $q.all([
+        $http.get('/answers'),
+        $http.get('/questions')
+      ])
+      .then(function(results) {
+        var data = [];
+        angular.forEach(results, function(result){
+          data = data.concat(result.data);
+        });
+        return data;
+      });
+    }
+  };
 }]);
