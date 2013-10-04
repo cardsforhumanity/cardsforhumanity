657247ecf131bddf9f5a7cc7affc2788bc5e3597
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-03 19:48:38 -0700

    removed unnecessary variables from controller

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,13 +4,11 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
       console.log('test!');
     });
 }]).controller('CardController', ['$scope', 'QuestionService', 'AnswerService', function($scope, QuestionService, AnswerService) {
-  $scope.question = {};
   QuestionService.getQuestions()
     .then(function(data) {
       $scope.question = data;
     });
 
-  $scope.answers = [];
   AnswerService.getAnswers()
     .then(function(data) {
       $scope.answers = data;
