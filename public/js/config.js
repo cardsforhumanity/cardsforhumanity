f1e52eef2eec5ca34ae28b5c7c8bc88c76619eaf
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-03 19:27:40 -0700

    merging data locally

--- public/js/config.js
+++ public/js/config.js
@@ -1,33 +1,33 @@
 //Setting up route
-window.app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/', {
-            templateUrl: 'views/index.html'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
-    }
-]);
+// window.app.config(['$routeProvider',
+//     function($routeProvider) {
+//         $routeProvider.
+//         when('/', {
+//             templateUrl: 'views/index.html'
+//         }).
+//         otherwise({
+//             redirectTo: '/'
+//         });
+//     }
+// ]);
 
 //Setting HTML5 Location Mode
-window.app.config(['$locationProvider',
-    function($locationProvider) {
-        $locationProvider.hashPrefix("!");
-    }
-]);
+// window.app.config(['$locationProvider',
+//     function($locationProvider) {
+//         $locationProvider.hashPrefix("!");
+//     }
+// ]);
 
 //Setting up SafeApply on rootScope
-window.app.run(['$rootScope', function($rootScope) {
-  $rootScope.safeApply = function(fn) {
-    var phase = this.$root.$$phase;
-    if(phase == '$apply' || phase == '$digest') {
-        if(fn && (typeof(fn) === 'function')) {
-            fn();
-        }
-    } else {
-        this.$apply(fn);
-      }
-  };
-}]);
+// window.app.run(['$rootScope', function($rootScope) {
+//   $rootScope.safeApply = function(fn) {
+//     var phase = this.$root.$$phase;
+//     if(phase == '$apply' || phase == '$digest') {
+//         if(fn && (typeof(fn) === 'function')) {
+//             fn();
+//         }
+//     } else {
+//         this.$apply(fn);
+//       }
+//   };
+// }]);
