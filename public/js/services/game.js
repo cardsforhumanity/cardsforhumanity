1a4cc8b751853bcfa5a78c9667c3cd1f045ccf6c
Hack Reactor Students <github@hackreactor.com>
2013-10-05 15:33:41 -0700

    Added local game object and update method.

--- public/js/services/game.js
+++ public/js/services/game.js
@@ -1,7 +1,27 @@
 angular.module('mean.system').factory('game', ['socket', function(socket){
 
+  var game = {
+    players: [],
+    czar: null,
+    playerLimit: null,
+    pointLimit: null,
+    state: null,
+    curQuestion: null,
+    timeLimits: {}
+  };
+
   socket.on('gameUpdate', function(data) {
     console.log(data);
+    if (data.state === 'game in progress') {
+      game.playerLimit = data.playerLimit;
+      game.pointLimit = data.pointLimit;
+      game.timeLimits = data.timeLimits;
+    }
+    game.players = data.players;
+    game.czar = data.czar;
+    game.state = data.state;
+    game.curQuestion = data.curQuestion;
   });
 
+  return game;
 }]);
