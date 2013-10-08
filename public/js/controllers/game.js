046316064e75333eec380d4acd0e23efaf7d615e
Hack Reactor Students <github@hackreactor.com>
2013-10-07 20:15:55 -0700

    Updated app view with live game data.

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -4,7 +4,7 @@ angular.module('mean.system')
 
     $scope.game = game;
 
-    // Use this in the HTML: <h2>{{game.curQuestion}}</h2>
+    // Use this in the HTML: <h2></h2>
     // <div ng-repeat="card in game.hand" ng-click="pickCard(card)">{{card.id}}</div>
 
 }])
