31d548f82a98c60f90d119a7a2a947464afb89c7
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-08 20:10:00 -0700

    Minor changes

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,13 +1,13 @@
 angular.module('mean.directives', [])
-  .directive('player', function(){
+  .directive('player', ['$animator', function ($animator){
     return{
       restrict: 'EA',
       templateUrl: '/views/player.html',
       link: function(scope, elem, attr){
-
+        $scope.toggle = true;
       }
     };
-  }).directive('answers', function() {
+  }]).directive('answers', function() {
     return {
       restrict: 'EA',
       templateUrl: '/views/answers.html',
