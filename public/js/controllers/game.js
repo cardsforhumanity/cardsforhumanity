d207d087fe95fc74cb7e01b96a79f2c423687d95
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-09 16:42:29 -0700

    Added icons to /img folder, set up ng-repeat to display images on signup page

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -4,6 +4,24 @@ angular.module('mean.system')
     $scope.winningCardPicked = false;
     $scope.game = game;
 
+
+    $scope.avatars = ['/img/chosen/E01.png',
+                      '/img/chosen/F01.png',
+                      '/img/chosen/FA04.png',
+                      '/img/chosen/FB03.png',
+                      '/img/chosen/FC01.png',
+                      '/img/chosen/FD01.png',
+                      '/img/chosen/FE01.png',
+                      '/img/chosen/FH03.png',
+                      '/img/chosen/FI02.png',
+                      '/img/chosen/H01.png',
+                      '/img/chosen/J01.png',
+                      '/img/chosen/M05.png',
+                      '/img/chosen/N02.png',
+                      '/img/chosen/N03.png',
+                      '/img/chosen/N04.png',
+                      '/img/chosen/N05.png'];
+
     $scope.pickCard = function(card) {
       game.pickCard(card);
       $scope.pickedCard = true;
