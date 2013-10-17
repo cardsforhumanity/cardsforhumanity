3d3277d861b0e639c484cae8b97e4c774dea84b1
silversteez <welcomematt@gmail.com>
2013-10-16 17:10:36 -0700

    Polish up game ending screens

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,6 +1,7 @@
 angular.module('mean.system')
   .filter('upperFirstLetter', function() {
     return function(input) {
+      input = input || 'Name';
       return input.charAt(0).toUpperCase() + input.slice(1);
-    }
+    };
   });
