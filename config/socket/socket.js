c91c52ab1d0cc4efcd0d754632cdd3dc36528c9e
William Ngo <will@mrngoitall.net>
2013-10-19 19:09:27 -0700

    Fixed timestamp format for logs

--- config/socket/socket.js
+++ config/socket/socket.js
@@ -1,6 +1,6 @@
 var Game = require('./game');
 var Player = require('./player');
-require("console-stamp")(console, "m/dd HH:mm:ss");
+require("console-stamp")(console, "m/dd HH:MM:ss");
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
