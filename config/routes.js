4f52db9c1928014c96e0c8f398ea7a513c493fbe
Will Ngo <wngo87@gmail.com>
2015-04-28 23:40:04 -0700

    Upgrade passport-facebook

--- config/routes.js
+++ config/routes.js
@@ -25,7 +25,7 @@ module.exports = function(app, passport, auth) {
 
     //Setting the facebook oauth routes
     app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email', 'user_about_me'],
+        scope: ['email'],
         failureRedirect: '/signin'
     }), users.signin);
 
